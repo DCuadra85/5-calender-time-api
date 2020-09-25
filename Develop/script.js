5bb1e4db6bb25ec7ae42219101cd7e7cde1785b4
Daniel Cuadra <daniel.cuadra85@gmail.com>
2020-09-25 09:29:45 -0700

    create calendar baseline

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,26 @@ var dateTime = [ '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm
 console.log(dateTime);
 
 var dateToday = moment().format("dddd, MMM Do");
-console.log(dateToday)
+// console.log(dateToday, "today's date test")
 $('#currentDay').append(dateToday);
 
-var momentTime = moment().format('LT');
-console.log(momentTime);
+function dynamicTime(){
 
+}
+
+function timeClock() {
+    var momentTime = moment().format('LT');
+    console.log(momentTime);
+    for(var i = 0; i < dateTime.length; i++){
+        $('.container').append(`
+        <div class= "row">
+        <div class = "hour col-1">${dateTime[i]}</div>
+        <div class = "present col-8"></div>
+        <div class = "saveBtn col-1"></div>
+      </div>
+        `);
+    }
+}
+
+timeClock()
+//date time array, append to bootstrap date time
